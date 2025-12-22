80e448482334e415dffdfe62cb9639c33e9c090a
Seth Smigelski <51036602+SethSmigelski@users.noreply.github.com>
2025-12-21 20:39:42 -0800

    Add block border thickness to CSS variables

--- src/save.js
+++ src/save.js
@@ -30,7 +30,8 @@ const {
 		'--seo44-link-radius': layout === 'horizontal' && linkBorderRadius ? `${linkBorderRadius}px` : undefined,
 
 		// sticky positioning
-    	'--seo44-sticky-offset': isSticky ? `${stickyOffset}px` : undefined
+    	'--seo44-sticky-offset': isSticky ? `${stickyOffset}px` : undefined,
+		'--seo44-block-border-thickness': borderWidth,
 	};
 	
 	const ListTag = listStyle === 'ol' ? 'ol' : 'ul';
